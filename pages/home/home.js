537718a2043dae737050906e9d3ff3a60f3ac018
xwt <710323287@qq.com>
2019-01-21 17:02:59 +0800

    banner测试

---
+++ pages/home/home.js
@@ -0,0 +1,78 @@
+// pages/home/home.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
+    indicatorDots: true,
+    vertical: false,
+    autopaly: false,
+    circular: true,
+    interval: 2000,
+    duration: 500,
+    perviousMargin: 0,
+    nextMargin: 0,
+    imgUrls: ['http://www.afinance.cn/new/UploadFiles_2266/201203/20120331202316974.jpg',
+      'http://dc.yesky.com/uploadImages/2010/057/ZJ62AI95JTQH_26.jpg',
+      'http://sh.eastday.com/images/thumbnailimg/month_1605/201605200248492311.jpg'
+    ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
